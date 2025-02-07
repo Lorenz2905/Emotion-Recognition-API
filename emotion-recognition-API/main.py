327d32c6a221dc6c0fc4d99e7b31be52f4cb4c28
Lorenz Neugebauer <152261927+Lorenz2905@users.noreply.github.com>
2025-02-07 08:14:28 +0100

    fix main.py

--- emotion-recognition-API/main.py
+++ emotion-recognition-API/main.py
@@ -27,4 +27,4 @@ app.include_router(upload_file_endpoint, tags=["Image Analysis"])
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="127.0.0.1", port=8000, reload=True)
+    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
